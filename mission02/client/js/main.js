bb246ddb47bb0e87952e40b45e9dfaaf62ce9090
NewBsk <pwo1101@hanmail.net>
2023-07-23 16:33:12 +0900

    과제 클릭 이벤트 활성화, nav 클릭시 BgColor 변경, Img 변경, NameText 변경

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -6,6 +6,29 @@
 
 const nav = getNode(".nav");
 
+function setBgColor(index) {
+  //배경색 변경( colorB의 기본값은 #000 으로 한다 )
+  //elem.style.background = `linear-gradient(to bottom, 'colorA','colorB')`;
+  const BgColor = getNode("body");
+  const colorA = data[index - 1].color[0];
+  const colorB = data[index - 1].color[1];
+
+  css(BgColor, "background", `linear-gradient(to bottom, ${colorA},${colorB}`);
+}
+function setImg(index) {
+  // target.src = `./assets/${data.name}.jpeg`;
+  // target.alt = data.alt
+  const visual = getNode(".visual img");
+  attr(visual, "src", `./assets/${data[index - 1].name}.jpeg`);
+  attr(visual, "alt", `${data[index - 1].alt}`);
+}
+
+function setNameText(index) {
+  // target.textContent = data.name;
+  const name = getNode(".nickName");
+  name.textContent = data[index - 1].name;
+}
+
 function handleSlider(e) {
   e.preventDefault();
 
@@ -14,9 +37,15 @@ function handleSlider(e) {
 
   if (!target) return;
 
+  const index = attr(target, "data-index");
+
   list.forEach((li) => removeClass(li, "is-active"));
 
   addClass(target, "is-active");
+
+  setImg(index);
+  setNameText(index);
+  setBgColor(index);
 }
 
 nav.addEventListener("click", handleSlider);
